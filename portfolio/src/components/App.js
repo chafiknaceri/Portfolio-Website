4107f58a941f93c5f8ffcc398cfbbba1464709f5
chafiknaceri <chafsnaceri@gmail.com>
2019-10-22 21:39:07 +0100

    started creating starter components

---
+++ portfolio/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                Chafiks Portfolio
+            </div>    
+          );
+    };
+}
+
+export default App;
+
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header';
-class App extends React.Component{
-    render(){
+import App from './components/App';
 
 
-        return(
-            <div >
-                <Header/>
-                <div className="container-fluid"><h1>Chafik Portfolio</h1></div>
-            </div>
-        );
-    }
-}
 
 ReactDOM.render(
     <App/>,
     document.querySelector("#root")
+
 );
